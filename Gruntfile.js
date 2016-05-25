646f8e7d85913c13e22801264f15e946f54c9c60
cartifon <ciceroartifon@gmail.com>
2016-05-25 15:30:34 -0300

    Adicionando 'jshint', 'csslint', 'autoprefixer'

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,34 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        jshint: {
+            all: ['javascript/scripts.js']
+        },
+        csslint: {
+            all: ['css/reset.css', 'css/style.css']
+        },
         cssmin: {
             dist: {
                 files: {
                     'css/style.min.css': ['css/reset.css', 'css/style.css']
                 }
             }
+        },
+        autoprefixer: {
+            all: {
+                src: 'css/style.min.css'
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     grunt.registerTask('default', [
-        'cssmin'
+        'jshint', // verifica os erros de digitação no javascript
+        'csslint', // verifica os erros de digitação no css
+        'cssmin', // minifica os arquivos
+        'autoprefixer' // prefixa as classes CSS para vários browsers
     ]);
 };
