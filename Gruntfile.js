7e7e3b4f7d742ae9faa0cae7f0b1093b0b5fe36a
cartifon <ciceroartifon@gmail.com>
2016-05-25 16:06:25 -0300

    Mudando as compilações do CSS para aplicar o less e ele mesmo vai mitificar os arquivos.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,41 +3,53 @@ module.exports = function(grunt) {
         jshint: {
             all: ['javascript/scripts.js']
         },
+        // csslint: {
+        //     all: ['css/reset.css', 'css/style.css']
+        // },
+        // cssmin: {
+        //     dist: {
+        //         files: {
+        //             'css/style.min.css': ['css/reset.css', 'css/style.css']
+        //         }
+        //     }
+        // },
+        // autoprefixer: {
+        //     all: {
+        //         src: 'css/style.min.css'
+        //     }
+        // }
+        less: {
+            dist: {
+                files: {
+                    'css/style.min.css': ['less/style.less', 'less/mixins.less']
+                }
+            },
+            options: {
+                compress: true
+            }
+        },
         concat: {
             dist: {
                 files: {
                     'javascript/all.js': ['javascript/scripts.js', 'javascript/plugins.js']
                 }
             }
-        },
-        csslint: {
-            all: ['css/reset.css', 'css/style.css']
-        },
-        cssmin: {
-            dist: {
-                files: {
-                    'css/style.min.css': ['css/reset.css', 'css/style.css']
-                }
-            }
-        },
-        autoprefixer: {
-            all: {
-                src: 'css/style.min.css'
-            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-autoprefixer');
+    // grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    // grunt.loadNpmTasks('grunt-contrib-csslint');
+    // grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     grunt.registerTask('default', [
         'jshint', // verifica os erros de digitação no javascript
-        'concat', // concatena os arquivos js
-        'csslint', // verifica os erros de digitação no css
-        'cssmin', // minifica os arquivos
-        'autoprefixer' // prefixa as classes CSS para vários browsers
+        // 'csslint', // verifica os erros de digitação no css
+        // 'cssmin', // minifica os arquivos
+        // 'autoprefixer' // prefixa as classes CSS para vários browsers
+        'less',
+        'concat' // concatena os arquivos js
     ]);
 };
